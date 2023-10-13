db4ee41cc8a7f520647bb24f0efed06fd4c3801a
Stan <stan@stansa.dev>
2023-10-13 14:34:28 -0700

    Scrape can gather a the first page of problems, but doesn't go to the rest

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 from scrape import scrape_description_and_solution
+from scrape import scrape_for_problem_ids
 from scrape_openai import request_hint
 from aws_dyn_put import put_problem_hint_in_dyn_table
 
-EX_PID = 'longest-substring-without-repeating-characters'
-
 def main():
-    problem_description, problem_solution = scrape_description_and_solution(EX_PID)
-    print("====PROBLEM DESCRIPTION====")
-    print(problem_description)
-    print("====PROBLEM SOLUTION====")
-    print(problem_solution)
+    problem_ids = scrape_for_problem_ids()
+    if problem_ids is None:
+        print("Error scraping for problem ids could not get any problem ids")
+        return
+    success_pids_set = set()
+    for pid in problem_ids:
+        problem_description, problem_solution = scrape_description_and_solution(pid)
+        print("====PROBLEM DESCRIPTION====")
+        print(problem_description)
+        print("====PROBLEM SOLUTION====")
+        print(problem_solution)
 
-    print ("Requesting hint from OpenAi...")
+        print ("Requesting hint from OpenAi...")
 
-    hint = request_hint(problem_description, problem_solution)
-    print("====HINT====")
-    print(hint)
+        hint = request_hint(problem_description, problem_solution)
+        print("====HINT====")
+        print(hint)
 
-    success = put_problem_hint_in_dyn_table(EX_PID, hint)
-    if success:
-        print("Successfully put hint in table")
-    else:
-        print("Error putting hint in table")
+        success = put_problem_hint_in_dyn_table(pid, hint)
+        if success:
+            print("Successfully put hint in table for problem id: {}".format(pid))
+            success_pids_set.add(pid)
+        else:
+            print("Error putting hint in table for problem id: {}".format(pid))
+
+    print("Successfully put hints in table for problem ids:\n{}".format(list(success_pids_set)))
 
 if __name__ == '__main__':
     main()
--- scrape.py
+++ scrape.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import *
 
+MAX_NUM_PROBLEMS_TO_SCRAPE = 25
+
 def problem_desc_url(pid):
     return 'https://leetcode.com/problems/{}/description/'.format(pid)
 
@@ -13,27 +15,41 @@ def problem_all_solutions_url(pid):
 def problem_single_solution_url(pid, sid):
     return 'https://leetcode.com/problems/{}/solutions/{}/'.format(pid, sid)
 
+# Const problemset all url
+PROBLEMSET_ALL_URL = 'https://leetcode.com/problemset/all/'
+
+def init_driver():
+    options = Options()
+    options.add_argument("--headless") # Runs Chrome in headless mode.
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.implicitly_wait(10)
+    driver.set_page_load_timeout(10)
+    print("Started Browser and Driver")
+    return driver
+
 def scrape_description_and_solution(pid):
-    print("START scrape")
+    print("START scrape desc and sol for problem id: {}".format(pid))
+    problem_description = None
+    problem_solution = None
     try:
         # Init driver
-        options = Options()
-        options.add_argument("--headless") # Runs Chrome in headless mode.
-        driver = webdriver.Chrome(chrome_options=options)
-        print("Started Browser and Driver")
+        driver = init_driver()
+
+        print("Going to problem description page")
         driver.get(problem_desc_url(pid))
 
         # Get the text content of the element with attribute data-track-load="description_content"
-        driver.implicitly_wait(10)
         element = driver.find_element_by_xpath("//div[@data-track-load='description_content']")
         problem_description = element.text
 
+        print("Going to problem all solutions page")
         # Go to the page with all the problem solutions
         driver.get(problem_all_solutions_url(pid))
 
         # Get the first a tag with attribute href starting with /problems/${problemId}/solutions/* and does not equal /problems/${problemId}/solutions/
         solution_link = driver.find_element_by_xpath("//a[starts-with(@href, '/problems/{}/solutions/') and not(@href='/problems/{}/solutions/')]".format(pid, pid))
 
+        print("Going to problem solution page")
         # Follow the link to the solution page
         solution_link.click()
 
@@ -41,10 +57,52 @@ def scrape_description_and_solution(pid):
         element = driver.find_element_by_xpath("//div[@class='break-words']")
         problem_solution = element.text
 
-        return problem_description, problem_solution
-
     except Exception as e:
         print("Error: ", e)
     finally:
         driver.quit()
         print("END scrape")
+
+    return problem_description, problem_solution
+
+def scrape_for_problem_ids():
+    "Returns a list of problem ids (strings) by scraping leetcode problem set page"
+
+    set_of_problem_ids = set()
+
+    try:
+        driver = init_driver()
+        driver.get(PROBLEMSET_ALL_URL)
+
+        driver.save_screenshot('screenshot.png')
+
+        page_index = 0
+        while page_index < MAX_NUM_PROBLEMS_TO_SCRAPE:
+            # Get all the a tags with attribute href starting with /problems/* and is not equal to /problems/
+            problem_links = driver.find_elements_by_xpath("//a[starts-with(@href, '/problems/') and not(@href='/problems/')]")
+            for link in problem_links:
+                try:
+                    problem_id = link.get_attribute('href').split('/')[-2]
+                    print("Adding problem id: {} to set".format(problem_id))
+                    set_of_problem_ids.add(problem_id)
+                except:
+                    print("Saw a link not work")
+                    continue
+
+            # If more pages to scrape, click the next page button and repeat
+            # The next page button has attribute aria-label="next"
+            # if it has attribute disabled, then there are no more pages to scrape
+            # next_page_button = driver.find_element_by_xpath("//button[@aria-label='next']")
+            break
+            if not next_page_button.get_attribute('disabled'):
+                next_page_button.click()
+            else:
+                break
+            page_index += 1
+
+    except Exception as e:
+        print("Error: ", e)
+        raise e
+
+    return list(set_of_problem_ids)
+
